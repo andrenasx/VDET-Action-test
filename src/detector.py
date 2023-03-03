a4a42de594d7a1d2d503bfe5fbe545a8a561e8b1
andrenasx <andre2000.an@gmail.com>
2023-03-03 19:15:55 +0000

    Initial VDET files

---
+++ src/detector.py
@@ -0,0 +1,116 @@
+import json
+import torch
+
+from transformer import getModel, getTokenizer, getMultilabelBinarizer, process_sequence, get_labels
+from utils import clean_code, flatten_list, remove_duplicate_labels, get_file_methods
+
+p_model = "./models/model.bin"
+p_mlb = "./models/multilabelbinarizer.pt"
+
+model = getModel(p_model)
+tokenizer = getTokenizer()
+mlb = getMultilabelBinarizer(p_mlb)
+
+
+""" def section(code):
+    req = request.get_json(force=True)
+
+    list_labels = []
+    list_lines = []
+
+    code = clean_code(req['code'])
+    lines = '-'.join(str(e) for e in req['lines'])
+
+    encodings = tokenizer.encode_plus(
+        code,
+        add_special_tokens=False,
+        return_tensors='pt')
+
+    input_ids, attn_mask = process_sequence(encodings)
+
+    # Stack lists so that it can be passed to the transformer
+    stacked_input_ids = (torch.stack(input_ids, 0)).type(torch.LongTensor)
+    stacked_attn_masks = torch.stack(attn_mask, 0)
+
+    # Get predictions
+    outputs = model(ids=stacked_input_ids, mask=stacked_attn_masks)
+    labels = get_labels(mlb, outputs)
+
+    flatten = flatten_list(labels)
+    noDup_labels = remove_duplicate_labels(flatten)
+
+    list_labels.append(noDup_labels)
+    list_lines.append(lines)
+
+    results = [{"range": t, "labels": s}
+               for t, s in zip(list_lines, list_labels)]
+    # print(results)
+    return json.dumps(results) """
+
+
+def file():
+    target_file = "code/helloworld.java" # Add the path to your file
+    methods = get_file_methods(target_file)
+
+    with open("./sarif/template.sarif") as f:
+        results_sarif = json.load(f)
+
+    results = []
+    
+    for method in methods:
+        code = clean_code(method['code'])
+        methodname = method['name']
+        startline = method['startline']
+        endline = method['endline']
+
+        encodings = tokenizer.encode_plus(
+            code,
+            add_special_tokens=False,
+            return_tensors='pt')
+
+        input_ids, attn_mask = process_sequence(encodings)
+
+        # Stack lists so that it can be passed to the transformer
+        stacked_input_ids = (torch.stack(input_ids, 0)).type(torch.LongTensor)
+        stacked_attn_masks = torch.stack(attn_mask, 0)
+
+        # Get predictions
+        outputs = model(ids=stacked_input_ids, mask=stacked_attn_masks)
+        labels = get_labels(mlb, outputs)
+
+        flatten = flatten_list(labels)
+        noDup_labels = remove_duplicate_labels(flatten)
+
+        results.append({
+            "ruleId": "VDET/CWE-" + noDup_labels[0][0],
+            "message": {
+                "id": "default",
+                "arguments": [
+                    noDup_labels[0][0],
+                    str(round(float(noDup_labels[0][1])*100, 2))
+                ]
+            },
+            "locations": [
+                {
+                    "physicalLocation": {
+                        "artifactLocation": {
+                            "uri": "/home/andrenasx/Thesis/Action/" + target_file,
+                        },
+                        "region": {
+                            "startLine": startline,
+                            "endLine": endline,
+                        }
+                    }
+                }
+            ]
+
+        })
+
+    results_sarif['runs'][0].update({"results": results})
+
+    with open("./results.sarif", "w") as f:
+        json.dump(results_sarif, f, indent=2)
+
+#! Test
+if __name__ == '__main__':
+    file()
---
+++ src/utils.py
@@ -0,0 +1,121 @@
+import re
+import javalang
+
+#* Code related functions
+
+def remove_comments(string):
+    pattern = r"(\".*?\"|\'.*?\')|(/\*.*?\*/|//[^\r\n]*$)"
+    # first group captures quoted strings (double or single)
+    # second group captures comments (//single-line or /* multi-line */)
+    regex = re.compile(pattern, re.MULTILINE|re.DOTALL)
+    def _replacer(match):
+        # if the 2nd group (capturing comments) is not None,
+        # it means we have captured a non-quoted (real) comment string.
+        if match.group(2) is not None:
+            return "" # so we will return empty to remove the comment
+        else: # otherwise, we will return the 1st group
+            return match.group(1) # captured quoted-string
+    return regex.sub(_replacer, string)
+
+def clean_code(code):
+    # Remove comments
+    code = remove_comments(code)
+
+    # Replace consecutive newlines with a single newline in code, and newlines with spaces in code
+    code = code.strip()
+    code = re.sub(r'\s+',' ', code)
+
+    return code
+
+def flatten_list(zipped):
+    return [item for sublist in zipped for item in sublist] # Flatten list
+
+def remove_duplicate_labels(labels):
+    d = {}
+    for x, y in labels:
+        if y not in d:
+            d[y] = x
+    
+    return [(k, v) for k, v in d.items()]
+
+
+#* Java methods related functions
+## From: https://github.com/c2nes/javalang/issues/49#issuecomment-915417079
+
+def get_method_start_end(tree, method_node):
+    startpos  = None
+    endpos    = None
+    startline = None
+    endline   = None
+    for path, node in tree:
+        if startpos is not None and method_node not in path:
+            endpos = node.position
+            endline = node.position.line if node.position is not None else None
+            break
+        if startpos is None and node == method_node:
+            startpos = node.position
+            startline = node.position.line if node.position is not None else None
+    return startpos, endpos, startline, endline
+
+def get_method_text(codelines, startpos, endpos, startline, endline, last_endline_index):
+    if startpos is None:
+        return "", None, None, None
+    else:
+        startline_index = startline - 1 
+        endline_index = endline - 1 if endpos is not None else None 
+
+        # 1. check for and fetch annotations
+        if last_endline_index is not None:
+            for line in codelines[(last_endline_index + 1):(startline_index)]:
+                if "@" in line: 
+                    startline_index = startline_index - 1
+        meth_text = "<ST>".join(codelines[startline_index:endline_index])
+        meth_text = meth_text[:meth_text.rfind("}") + 1] 
+
+        # 2. remove trailing rbrace for last methods & any external content/comments
+        # if endpos is None and 
+        if not abs(meth_text.count("}") - meth_text.count("{")) == 0:
+            # imbalanced braces
+            brace_diff = abs(meth_text.count("}") - meth_text.count("{"))
+
+            for _ in range(brace_diff):
+                meth_text  = meth_text[:meth_text.rfind("}")]    
+                meth_text  = meth_text[:meth_text.rfind("}") + 1]     
+
+        meth_lines = meth_text.split("<ST>")  
+        meth_text  = "".join(meth_lines)                   
+        last_endline_index = startline_index + (len(meth_lines) - 1) 
+
+        return meth_text, (startline_index + 1), (last_endline_index + 1), last_endline_index
+
+
+def get_file_methods(target_file):
+    with open(target_file, 'r') as r:
+        codelines = r.readlines()
+        code_text = ''.join(codelines)
+
+    lex = None
+    tree = javalang.parse.parse(code_text)    
+    methods = []
+    for _, method_node in tree.filter(javalang.tree.MethodDeclaration):
+        startpos, endpos, startline, endline = get_method_start_end(tree, method_node)
+        method_text, startline, endline, lex = get_method_text(codelines, startpos, endpos, startline, endline, lex)
+
+        methods.append({"name": method_node.name, "code": method_text, "startline": startline, "endline": endline})
+
+        """ print("--- ### ---")
+        print(method_text)
+        print("Line range: ", startline, " - ", endline)
+        print("--- ### ---") """
+    
+    return methods
+
+#! Test
+if __name__ == "__main__":
+    methods = get_file_methods("./code/helloworld.java")
+    for method in methods:
+        print("--- ### ---")
+        print(method["name"])
+        print(method["code"])
+        print("Line range: ",method["startline"], " - ", method["endline"])
+        print("--- ### ---")
